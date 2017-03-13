b24fd8b7d72169060cb1f90bbe984e3d7e2a916a
plalo <plalo@ctechnologiesinc.com>
2017-03-13 16:52:08 -0400

    Initial Commit

---
+++ LoginADModule/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace LoginADModule
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.username = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.TextBox();
+            this.domain = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(112, 36);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(100, 20);
+            this.username.TabIndex = 2;
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(112, 62);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(100, 20);
+            this.password.TabIndex = 3;
+            // 
+            // domain
+            // 
+            this.domain.Location = new System.Drawing.Point(112, 88);
+            this.domain.Name = "domain";
+            this.domain.Size = new System.Drawing.Size(100, 20);
+            this.domain.TabIndex = 4;
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox domain;
+    }
+}
+
---
+++ LoginADModule/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Security.Permissions;
+using System.DirectoryServices;
+using System.DirectoryServices.ActiveDirectory;
+using System.DirectoryServices.AccountManagement;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace LoginADModule
+{
+    public partial class Form1 : Form
+    {
+        // Variables
+        public bool authenticated = false;
+        public string currentUser;
+
+        // Used for bool to get groups
+        [DllImport("advapi32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool RevertToSelf();
+
+        // Get the domain name, just in case domain name changes, didn't want to hard code
+        private string GetSystemDomain()
+        {
+            try
+            {
+                return Domain.GetComputerDomain().ToString().ToLower();
+            }
+            catch (Exception e)
+            {
+                e.Message.ToString();
+                return string.Empty;
+            }
+        }
+
+        // Search if user/pass combo exists
+        private bool GetDirectorySearcher(string usr, string pwd, string dom)
+        {
+            DirectoryEntry entry = new
+                DirectoryEntry("LDAP://" + dom, usr, pwd);
+            try
+            {
+                object obj = entry.NativeObject;
+                DirectorySearcher search = new DirectorySearcher(entry);
+                search.Filter = "(SAMAccountName=" + usr + ")";
+                search.PropertiesToLoad.Add("cn");
+            }
+            catch (DirectoryServicesCOMException e)
+            {
+                e.ToString();
+                return false;
+            }
+            return true;
+        }
+
+        // Authenticate
+        public bool GetUserInformation(string user, string pass)
+        {
+            if (user.Trim().Length != 0 && pass.Trim().Length != 0)
+            {
+                string dom = GetSystemDomain();
+                if (GetDirectorySearcher(user, pass, dom))
+                {
+                    authenticated = true;
+                    currentUser = user;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        // Return Username
+        public string getUsername()
+        {
+            return currentUser;
+        }
+
+        // Return which group they belong to
+        public string getGroup()
+        {
+            return getAuthorizationGrps(currentUser);
+        }
+
+        // Get which group they belong to
+        public string getAuthorizationGrps(string userName)
+        {
+            List<string> grps = new List<string>();
+            string domain_name = GetSystemDomain();
+            PrincipalContext context = new PrincipalContext(ContextType.Domain, domain_name); ;
+
+            try
+            {
+                var currentUser = UserPrincipal.FindByIdentity(context, IdentityType.SamAccountName, userName);
+                RevertToSelf();
+                PrincipalSearchResult<Principal> groups = currentUser.GetGroups();
+                IEnumerable<string> groupNames = groups.Select(x => x.SamAccountName);
+                foreach (var name in groupNames)
+                {
+                    grps.Add(name.ToString());
+                }
+                string groupLists = string.Join(", ", grps);
+                return groupLists;
+            }
+            catch (Exception ex)
+            {
+                string error = ex.ToString();
+                return error;
+            }
+        }
+    }
+}
